639c87121fcd632d63dba9e47557833a45fec70a
Eric Muyser <eric@muyser.com>
2019-07-30 12:42:17 +0900

    Exports

--- packages/hyper-ui/src/components/index.js
+++ packages/hyper-ui/src/components/index.js
@@ -1,7 +1,5 @@
 import * as PromotionBoxItem from './promotion-box/item'
 import * as PromotionBoxList from './promotion-box/list'
 
-export default {
-    PromotionBoxItem,
-    PromotionBoxList
-}
+export PromotionBoxItem
+export PromotionBoxList
