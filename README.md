3ab0d9bd3f0e50a78571d3cd982aed0958e13fd6
Eric Muyser <eric@muyser.com>
2019-07-30 16:56:42 +0900

    Export packages

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Hyper UI
+
+## Getting Started
+
+```bash
+
+npm install @ericmuyser/hyper-ui
+
+# deps
+npm install moment
+```
--- packages/hyper-ui/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import * as PromotionBoxItem from './promotion-box/item'
-import * as PromotionBoxList from './promotion-box/list'
-
-export PromotionBoxItem
-export PromotionBoxList
