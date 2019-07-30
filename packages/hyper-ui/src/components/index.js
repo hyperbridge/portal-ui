5ecb6065743a9716c9c3c7a179d5a11a872ef16c
Eric Muyser <eric@muyser.com>
2019-07-30 12:00:24 +0900

    Testing exports

---
+++ packages/hyper-ui/src/components/index.js
@@ -0,0 +1,7 @@
+import * as PromotionBoxItem from './promotion-box/item'
+import * as PromotionBoxList from './promotion-box/list'
+
+export default {
+    PromotionBoxItem,
+    PromotionBoxList
+}
---
+++ packages/hyper-ui/src/index.js
@@ -0,0 +1,12 @@
+import * as components from './components'
+import * as directives from './directives'
+
+const init = function () {
+
+}
+
+export default {
+    ...components,
+    directives,
+    init
+}
